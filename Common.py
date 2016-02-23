dd72976d2485527050020b972625964a9f6ac151
Git-vimal <vimal.patidar@ymail.com>
2016-02-23 23:01:42 +0530

    Update Common.py

--- Common.py
+++ Common.py
@@ -2,5 +2,3 @@ base_url = 'http://tul1engttrw1.corporate.local/'
 user = ''
 password = ''
 project = ''
-
-data = {'section_id': 499781, 'title': 'MDS sample', 'refs': None, 'estimate': None, 'estimate_forecast': None, 'type_id': 2, 'custom_expected': '200 ok', 'custom_preconds': None, 'priority_id': 4, 'milestone_id': None}
